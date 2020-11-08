7eaf81ea6e71b0c0c2ca0d5246ac1682a45f630c
Simran <mdrsimran@gmail.com>
2020-11-08 15:32:07 -0600

    commit tree 89e00323f80641236d0422fc761ac70bd106d44d

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,16 @@
 class Meal
+    attr_accessor :waiter, :customer, :total, :tip
+    @@all=[]
 
+    def initialize(waiter, customer, total,tip=0)
+        @total= total
+        @waiter= waiter
+        @customer = customer
+        @tip= tip
+        @@all<<self
+    end
+
+    def self.all
+        @@all
+    end
 end
